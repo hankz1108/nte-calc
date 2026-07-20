b5cee99ec90eeafdf7795e364144378af68df861
hankz <hankz1108@gmail.com>
2026-07-20 22:51:07 +0800

    feat: ✨  微調文本與連結

--- docs/components/footer.js
+++ docs/components/footer.js
@@ -7,7 +7,7 @@ window.NTE.components.Footer = {
       <aside>
         <p class="text-sm">異環（NTE）素材計算器</p>
         <p class="text-xs">數值僅供參考，實際以遊戲內為準。</p>
-        <p class="text-xs">Made by <a href="https://hankz1108.github.io/" target="_blank" rel="noopener" class="link link-hover">hankz</a></p>
+        <p class="text-xs">Made by <a href="https://hankz1108.github.io/about/" target="_blank" rel="noopener" class="link link-hover">hankz</a></p>
       </aside>
     </footer>
   `,
